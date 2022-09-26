6059331febf039ccedcec19aee8952232d3aa71b
Adjarabet\k.khukhia <k.khukhia@adjarabet.com>
2022-09-26 14:12:16 +0400

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    
+
     $('.share-btn').on('click', function () {
         $(this).toggleClass('show-social');
     });
-    
+
     $('.burger-menu').on('click', function () {
         $('.main-menu').addClass('show-main-menu');
     });
-    
+
     $('.close-menu').on('click', function () {
         $('.main-menu').removeClass('show-main-menu');
     });
@@ -41,7 +41,60 @@ $(document).ready(function () {
     //     $(this).prev('.show-pass').show();
     // });
 
-    // $('.select2').select2({
-    //     minimumResultsForSearch: -1
-    // });
+    if ($('.select2').length > 0) {
+        $('.select2').select2({
+            minimumResultsForSearch: -1
+        });
+
+        $('.select2').on('change', function (e) {
+            if ($(this).next('.select2-container').find('.select2-selection__rendered').attr('title') !== '') {
+                $(this).next('.select2-container').next('.hide-label').addClass('active-label');
+            }
+        });
+
+        $('.select2').on('select2:opening', function (e) {
+            $(this).next('.select2-container').next('.select-label').addClass('active-label');
+        });
+    
+        $('.select2').on('select2:closing', function (e) {
+            if($(this).next('.select2-container').find('.select2-selection__rendered').attr('title')) {
+            } else { 
+                $(this).next('.select2-container').next('.select-label').removeClass('active-label');
+            }
+        });
+    }
+    if ($('.datepicker').length > 0) {
+
+        $.datepicker._updateDatepicker_original = $.datepicker._updateDatepicker;
+        $.datepicker._updateDatepicker = function (inst) {
+            $.datepicker._updateDatepicker_original(inst);
+            var afterShow = this._get(inst, 'afterShow');
+            if (afterShow)
+                afterShow.apply((inst.input ? inst.input[0] : null));  // trigger custom callback
+        }
+        $(".datepicker").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dayNames: ["კვირა", "ორშაბათი", "სამშაბათი", "ოთხშაბათი", "ხუთშაბათი", "პარასკევი", "შაბათი"],
+            monthNames: ["იანვარი", "თებერვალი", "მარტი", "აპრილი", "მაისი", "ივნისი", "ივლისი", "აგვისტო"],
+            dayNamesMin: ["კვი", "ორშ", "სამ", "ოთხ", "ხუთ", "პარ", "შაბ"],
+            monthNamesShort: ["იანვარი", "თებერვალი", "მარტი", "აპრილი", "მაისი", "ივნისი", "ივლისი", "აგვისტო", "სექტემბერი", "ოქტომბერი", "ნოემბერი", "დეკემბერი"],
+            firstDay: 1,
+            afterShow: function () {
+                $('#ui-datepicker-div').addClass('show');
+                $('.ui-datepicker select').select2({
+                    minimumResultsForSearch: -1,
+                    theme: "datepicker"
+                });
+                $('.ui-datepicker select').on("select2:open", function () {
+                    $('.select2-results').perfectScrollbar();
+                });
+            },
+        });
+    }
+
+    if($('.ps').length > 0) {
+        $('.ps').perfectScrollbar();
+    }
+
 });
